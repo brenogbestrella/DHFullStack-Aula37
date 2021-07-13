722a221b13abcb919ea490445517d10d217c6dbf
Breno Estrella <brenogbestrella@gmail.com>
2021-07-12 21:32:40 -0300

    Commit aula 46

--- users-frontend/src/App.js
+++ users-frontend/src/App.js
@@ -1,40 +1,37 @@
+import { MdNavigateBefore, MdNavigateNext  } from "react-icons/md"
+
+
 import './App.css';
 import { useState } from "react";
 
-import Header from "./components/header"
-import Footer from "./components/footer"
+import Pagination from "./components/pagination"
+
+// import Header from "./components/header"
+// import Footer from "./components/footer"
 
 import { getCepData } from "./services/viacep";
 
 function App() {
 
-  const [cep, setCep] = useState("22270000");
+  const [cep, setCep] = useState("");
   const [logradouro, setLogradouro] = useState("");
-  const [mustShow, setMustShow] = useState(true);
+  const [page, setPage] = useState(1)
+
 
   async function onBlurCep() {
     const data = await getCepData(cep)
     setLogradouro(data.logradouro);
   };
 
-  function onClickHide() {
-    setMustShow(!mustShow);
-  }
-
-
   return (
     <div className="background" id="background">
-      <Header />
-      <button type="button" onClick={onClickHide}>Esconder campos</button>
       <form id="formSignup">
-        {mustShow && (
-          <>
+
             <span id="userErrorSignup" className="error">Usuário incorreto</span>
             <input id="userSignup" type="text" placeholder="Digite seu usuário"/>
             <span id="passwordErrorSignup" className="error">Senha incorreta</span>
             <input id="passwordSignup" type="password" placeholder="Digite sua senha"/>
-          </>
-        )}
+        
         <input 
           id="cep" 
           type="text" 
@@ -58,8 +55,13 @@ function App() {
         <input id="numero" type="text" placeholder="Digite seu número"/>
 
         <button id="signUpBtn" type="submit">Cadastrar</button>
+        
+        <Pagination 
+        page={page} 
+        onPageChange={(newPage) => setPage(newPage)}
+      />
       </form>
-      <Footer />
+      
     </div>
   );
 }
